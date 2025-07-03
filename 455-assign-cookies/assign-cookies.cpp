347fa155be29f80906ea5aed69a7207b86cd2b10
Shreenita Saha <shreenita2003@gmail.com>
2025-07-03 23:49:31 +0530

    Time: 4 ms (87.48%) | Memory: 44.8 MB (43.60%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -7,10 +7,10 @@ public:
         int i = 0, j = 0;
         while (i < g.size() && j < s.size()) {
             if (s[j] >= g[i]) {
-                // Bachha khush ho gaya
+                
                 i++;
             }
-            // Har case me cookie to use ho jaayegi
+           
             j++;
         }
         return i; // i children are content
