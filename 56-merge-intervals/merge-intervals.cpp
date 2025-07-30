fcc7f0f1115c6d5728b73eec28bf6c51085d34f2
Shreenita Saha <shreenita2003@gmail.com>
2025-07-30 21:16:33 +0530

    Time: 4 ms (69.51%) | Memory: 23.8 MB (80.12%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        int n=intervals.size();
+        sort(intervals.begin(),intervals.end());
+        vector<int> curr=intervals[0];
+        for(int i=0;i<n;i++) {
+            if(intervals[i][0]<=curr[1]){
+                curr[1]=max(curr[1],intervals[i][1]);
+                //already sorted, so starting point u know,just see ending point
+            }
+            else{
+                res.push_back(curr);
+                curr=intervals[i];
+            }
+        }
+        res.push_back(curr);
+        
+        return res;
+        
+    }
+};
